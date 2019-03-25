a3cf850a3b20d504094ff33b48b3f4c172a6ae2d
Andres Diaz <diazgtz1204@gmail.com>
2019-03-24 18:32:40 -0600

    ultimos cambios

---
+++ Adminsitrador/principal.py
@@ -0,0 +1,10 @@
+def mostrar_menu():
+    print ("""
+    Sleccione la opcion que desea realizar 
+
+    1.- Ver todos los proyetos
+    2.- Editar un proyecto 
+    3.- Eliminar un proyecto 
+    4.- Crear un proyecto 
+
+    """)
